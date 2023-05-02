ba102c15662a1e2c867c8e5547cf9c8834b6027c
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2023-05-02 12:31:23 +0200

    dependency chain fix

--- main.tf
+++ main.tf
@@ -39,6 +39,10 @@ resource "aws_s3_bucket_public_access_block" "b" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.b,
+  ]
 }
 
 
